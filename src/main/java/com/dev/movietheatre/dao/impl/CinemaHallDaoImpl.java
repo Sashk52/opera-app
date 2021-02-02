0662415a6a6eda95174757a2775d6d4f7a0544eb
Sashk52 <a.tsven@gmail.com>
2021-02-02 12:52:20 +0200

    added one missed correction

--- src/main/java/com/dev/movietheatre/dao/impl/CinemaHallDaoImpl.java
+++ src/main/java/com/dev/movietheatre/dao/impl/CinemaHallDaoImpl.java
@@ -36,8 +36,7 @@ public class CinemaHallDaoImpl implements CinemaHallDao {
 
     @Override
     public List<CinemaHall> getAll() {
-        try {
-            Session session = HibernateUtil.getSessionFactory().openSession();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<CinemaHall> getAllCinemaHallsQuery = session.createQuery("from CinemaHall",
                     CinemaHall.class);
             return getAllCinemaHallsQuery.getResultList();

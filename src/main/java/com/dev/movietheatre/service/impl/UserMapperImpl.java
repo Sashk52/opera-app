896d17d546d16a79b764df09e33dd73a3648ecb5
Sashk52 <a.tsven@gmail.com>
2021-02-17 00:09:46 +0200

    deleted method which not used & deleted password field from responseDto

--- src/main/java/com/dev/movietheatre/service/impl/UserMapperImpl.java
+++ src/main/java/com/dev/movietheatre/service/impl/UserMapperImpl.java
@@ -1,7 +1,6 @@
 package com.dev.movietheatre.service.impl;
 
 import com.dev.movietheatre.model.User;
-import com.dev.movietheatre.model.dto.UserRequestDto;
 import com.dev.movietheatre.model.dto.UserResponseDto;
 import com.dev.movietheatre.service.UserMapper;
 import org.springframework.stereotype.Component;
@@ -14,13 +13,5 @@ public class UserMapperImpl implements UserMapper {
         userResponseDto.setId(user.getId());
         userResponseDto.setEmail(user.getEmail());
         return userResponseDto;
-    }
-
-    @Override
-    public User dtoToUser(UserRequestDto userRequestDto) {
-        User user = new User();
-        user.setEmail(userRequestDto.getEmail());
-        user.setPassword(userRequestDto.getPassword());
-        return user;
     }
 }

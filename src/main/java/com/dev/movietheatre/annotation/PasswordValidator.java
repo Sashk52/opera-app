cd9b88b23f1e6865735714bc92a5e19fa641d496
Sashk52 <a.tsven@gmail.com>
2021-02-18 15:10:38 +0200

    changed parameter type and variable name in PasswordValidator

--- src/main/java/com/dev/movietheatre/annotation/PasswordValidator.java
+++ src/main/java/com/dev/movietheatre/annotation/PasswordValidator.java
@@ -4,7 +4,7 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import org.springframework.beans.BeanWrapperImpl;
 
-public class PasswordValidator implements ConstraintValidator<PasswordConstraint, String> {
+public class PasswordValidator implements ConstraintValidator<PasswordConstraint, Object> {
     private String password;
     private String repeatPassword;
 
@@ -13,9 +13,9 @@ public class PasswordValidator implements ConstraintValidator<PasswordConstraint
         this.repeatPassword = constraint.repeatPassword();
     }
 
-    public boolean isValid(String obj, ConstraintValidatorContext context) {
-        Object passwordField = new BeanWrapperImpl(obj).getPropertyValue(password);
-        Object passwordRepeatField = new BeanWrapperImpl(obj).getPropertyValue(repeatPassword);
+    public boolean isValid(Object user, ConstraintValidatorContext context) {
+        Object passwordField = new BeanWrapperImpl(user).getPropertyValue(password);
+        Object passwordRepeatField = new BeanWrapperImpl(user).getPropertyValue(repeatPassword);
         return passwordField != null & passwordField.equals(passwordRepeatField);
     }
 }
